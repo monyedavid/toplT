60a5cd684a82338722da9d911cb30a17f5037012
monyedavid <monyeodavid@gmail.com>
2022-09-08 01:39:03 +0100

    spec update    - use Gen

--- src/test/scala/org/topl/traffic/protocol/models/generators.scala
+++ src/test/scala/org/topl/traffic/protocol/models/generators.scala
@@ -2,6 +2,7 @@ package org.topl.traffic.protocol.models
 
 import io.circe.Json
 import org.scalacheck.Gen
+import Intersection._
 
 object generators {
 
@@ -143,5 +144,27 @@ object generators {
         )
       )
     )
+
+  def testGraphGen: Gen[WeightedGraph[Intersection]] = Gen.const(
+    Graph[Intersection]()
+      .addEdge("M1".fromStringUnsafe, WeightedEdge("G1".fromStringUnsafe, 60))
+      .addEdge("M1".fromStringUnsafe, WeightedEdge("C1".fromStringUnsafe, 160))
+      .addEdge("M1".fromStringUnsafe, WeightedEdge("D1".fromStringUnsafe, 170))
+      .addEdge("M1".fromStringUnsafe, WeightedEdge("J1".fromStringUnsafe, 50))
+      .addEdge("M1".fromStringUnsafe, WeightedEdge("B1".fromStringUnsafe, 250))
+      .addEdge("M1".fromStringUnsafe, WeightedEdge("D2".fromStringUnsafe, 360))
+      .addEdge("G1".fromStringUnsafe, WeightedEdge("C2".fromStringUnsafe, 20))
+      .addEdge("C2".fromStringUnsafe, WeightedEdge("C1".fromStringUnsafe, 40))
+      .addEdge("C1".fromStringUnsafe, WeightedEdge("C1".fromStringUnsafe, 210))
+      .addEdge("D1".fromStringUnsafe, WeightedEdge("S1".fromStringUnsafe, 320))
+      .addEdge("S1".fromStringUnsafe, WeightedEdge("B1".fromStringUnsafe, 210))
+      .addEdge("B1".fromStringUnsafe, WeightedEdge("S1".fromStringUnsafe, 260))
+      .addEdge("B1".fromStringUnsafe, WeightedEdge("D2".fromStringUnsafe, 90))
+      .addEdge("D2".fromStringUnsafe, WeightedEdge("B1".fromStringUnsafe, 140))
+      .addEdge("J1".fromStringUnsafe, WeightedEdge("D1".fromStringUnsafe, 110))
+      .addEdge("J1".fromStringUnsafe, WeightedEdge("K2".fromStringUnsafe, 190))
+      .addEdge("K2".fromStringUnsafe, WeightedEdge("J1".fromStringUnsafe, 160))
+      .addEdge("K2".fromStringUnsafe, WeightedEdge("D2".fromStringUnsafe, 90))
+  )
 
 }
